dd8f64e74d554bfb74b7afeb3f9969402cf119eb
mac <franquicidad@gmail.com>
2017-08-17 22:26:36 -0500

    new adjustments

--- app/src/main/java/com/example/mac/booklistingapp/MainActivity.java
+++ app/src/main/java/com/example/mac/booklistingapp/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.mac.booklistingapp;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.app.LoaderManager;
+
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -64,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 loaderManager.initLoader(EARTHQUAKE_LOADER_ID,null,this);
             }
         });
-    }
+    }·
 
     @Override
     public Loader<List<BookItems>> onCreateLoader(int i, Bundle bundle) {
